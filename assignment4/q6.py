f65309730de48d75fa3be0718eda1d2e0e699a58
Sudais <44402465+sudaisyasin@users.noreply.github.com>
2020-11-20 18:54:07 +0500

    Update q6.py

--- assignment4/q6.py
+++ assignment4/q6.py
@@ -1,3 +1,5 @@
+month_names = { 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December' }
+
 def popular_month(t):
     months = [0 for i in range(12)]
     for each in t:
